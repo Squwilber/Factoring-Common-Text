08dd975b76efaa3f55a9cde907459ed5f74cf26d
Gousemoodhin Nadaf <nadafgouse5@gmail.com>
2021-05-03 10:16:49 -0400

    Read input file and write data into output file (html)

--- source/src/createhtml.cpp
+++ source/src/createhtml.cpp
@@ -1,19 +1,29 @@
 #include <createhtml.h>
 #include <fsstream>
 #include <iostream>
+#include <string>
 using namespace std;
+
+const string headerTag = "!DOCTYPE html><html><head></head><body>";
+const string footerTag = "</body></html>";
 
 namespace FactoringCommonText {
 
 void createHTMLForGivenText() {
+  ofstream stream;
+  ifstream inputFile("textfile", std::ios::binary | std::ios::ate);
+  streamsize bufSize = file.tellg();
+  inputFile.seekg(0, std::ios::beg);
+  vector<char> inputBuffer(bufSize);
+  inputFile
+      .read(inputBuffer.data(), bufSize) // read input file contents
 
-  ofstream inputFile;
-  inputFile.open("output.html");
-  inputFile << "Insert starting tag of HTML";
-  inputFile << "Insert Contenets from input text file";
+  stream.open("output.html");
+  stream << headerTag;
+  stream.write(inputBuffer.data(), bufSize);
+  stream << footerTag;
 
-  inputFile << "Insert snd tag of HTML";
-  inputFile.close();
+  stream.close();
 }
 
 } // end of namespace FactoringCommonText
